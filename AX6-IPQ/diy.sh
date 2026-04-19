ca4edf7e362bc976d60d0cf30211f27b3b4905cb
OrdinaryJoys <90075145+OrdinaryJoys@users.noreply.github.com>
2026-04-19 12:56:52 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -67,3 +67,5 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 
 #修改主机名
 #sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
+
+chmod +x $GITHUB_WORKSPACE/openwrt/files/etc/uci-defaults/* 2>/dev/null
